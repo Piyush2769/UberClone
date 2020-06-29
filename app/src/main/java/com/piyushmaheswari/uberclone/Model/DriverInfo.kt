c7d239b16dcd173c8a815a18b63de0552851d576
Piyush Maheswari <piyush.maheswari@testbook.com>
2020-06-30 00:01:54 +0530

    Registration Completed

---
+++ app/src/main/java/com/piyushmaheswari/uberclone/Model/DriverInfo.kt
@@ -0,0 +1,8 @@
+package com.piyushmaheswari.uberclone.Model
+
+class DriverInfo {
+    var firstName: String = ""
+    var lastName: String = ""
+    var phoneNumber: String = ""
+    var rating = 0.0
+}
--- app/src/main/java/com/piyushmaheswari/uberclone/SplashScreenActivity.kt
+++ app/src/main/java/com/piyushmaheswari/uberclone/SplashScreenActivity.kt
@@ -2,17 +2,28 @@ package com.piyushmaheswari.uberclone
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.piyushmaheswari.uberclone.Model.DriverInfo
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+import kotlinx.android.synthetic.main.layout_register.*
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -89,7 +100,11 @@ class SplashScreenActivity : AppCompatActivity() {
 
                 override fun onDataChange(snapshot: DataSnapshot) {
                     if (snapshot.exists()) {
-                        Toast.makeText(this@SplashScreenActivity, "User already exists", Toast.LENGTH_LONG)
+                        Toast.makeText(
+                            this@SplashScreenActivity,
+                            "User already exists",
+                            Toast.LENGTH_LONG
+                        )
                             .show()
                     } else {
                         showRegisterLayout()
@@ -100,6 +115,76 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private fun showRegisterLayout() {
+
+        val builder = AlertDialog.Builder(this, R.style.DialogTheme)
+        val itemView = LayoutInflater.from(this).inflate(R.layout.layout_register, null)
+
+        val phoneNumber=itemView.findViewById<View>(R.id.phone_number) as TextInputEditText
+        val firstName=itemView.findViewById<View>(R.id.first_name) as TextInputEditText
+        val lastName=itemView.findViewById<View>(R.id.last_name) as TextInputEditText
+
+        val btnContinue=itemView.findViewById<Button>(R.id.btn_continue) as Button
+
+        if (FirebaseAuth.getInstance().currentUser!!.phoneNumber != null &&
+            !TextUtils.isDigitsOnly(FirebaseAuth.getInstance().currentUser!!.phoneNumber)
+        )
+            phoneNumber.setText(FirebaseAuth.getInstance().currentUser!!.phoneNumber)
+
+        builder.setView(itemView)
+        val dialog = builder.create()
+        dialog.show()
+
+        btnContinue.setOnClickListener {
+            if (TextUtils.isDigitsOnly(firstName.text.toString())) {
+                Toast.makeText(
+                    this@SplashScreenActivity,
+                    "Please Enter First Name",
+                    Toast.LENGTH_LONG
+                )
+                return@setOnClickListener
+            } else if (TextUtils.isDigitsOnly(lastName.text.toString())) {
+                Toast.makeText(
+                    this@SplashScreenActivity,
+                    "Please Enter Last Name",
+                    Toast.LENGTH_LONG
+                )
+                return@setOnClickListener
+            } else if (TextUtils.isDigitsOnly(phoneNumber.text.toString())) {
+                Toast.makeText(
+                    this@SplashScreenActivity,
+                    "Please Enter Phone Number",
+                    Toast.LENGTH_LONG
+                )
+                return@setOnClickListener
+            } else {
+                val model = DriverInfo()
+                model.firstName = firstName.text.toString()
+                model.lastName = lastName.text.toString()
+                model.phoneNumber = phoneNumber.text.toString()
+                model.rating = 0.0
+
+                driverInfoRef.child(FirebaseAuth.getInstance().currentUser!!.uid)
+                    .setValue(model)
+                    .addOnFailureListener {
+                        Toast.makeText(
+                            this@SplashScreenActivity,
+                            "" + it.message,
+                            Toast.LENGTH_LONG
+                        )
+                        dialog.dismiss()
+                        progress_bar.visibility = View.GONE
+                    }
+                    .addOnSuccessListener {
+                        Toast.makeText(
+                            this@SplashScreenActivity,
+                            "Registered Successfully",
+                            Toast.LENGTH_LONG
+                        )
+                        dialog.dismiss()
+                        progress_bar.visibility = View.GONE
+                    }
+            }
+        }
 
     }
 
@@ -137,3 +222,4 @@ class SplashScreenActivity : AppCompatActivity() {
         }
     }
 }
+
